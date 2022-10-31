e6515512d6feadaef897e5687968d97c1a21410a
Carlosramirz <103346462+Carlosramirz@users.noreply.github.com>
2022-10-31 18:59:36 -0300

    1
    1

---
+++ recursos/js/index.js
@@ -0,0 +1,8 @@
+/* Header */
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+}
